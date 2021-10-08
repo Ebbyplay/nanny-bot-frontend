1bb998a8d6701de2c7446fca5597702c618aae4e
ox7f <ox7f@protonmail.com>
2021-10-08 23:06:33 +0200

    nanyImage component

--- src/Components/NannyImage.js
+++ src/Components/NannyImage.js
@@ -1,31 +1,24 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import React from 'react';
-import { Col, Container, Image, Row } from 'react-bootstrap';
+import Image from 'react-bootstrap/Image';
 
 class NannyImage extends React.Component {
     render() {
         let imageStyle= {
             height: '100px',
             width: '100px',
-            backgroundColor: this.props.selectedImages.indexOf(this.props.image) !== -1 ? 'green' : 'red'
+            backgroundColor: this.props.selected.indexOf(this.props.image) !== -1 ? 'green' : 'red'
         };
 
         return (
             <>
-                <Container>
-                    <Row>
-                        <Col xs={6} md={4} onClick={this.props.click}>
-                            <Image 
-                                style={imageStyle} 
-                                src={this.props.image.path} 
-                                onClick={() => this.props.toggleImage(this.props.image)} 
-                                rounded 
-                            />
-                            {this.props.image.index}
-                        </Col>
-                    </Row>
-                </Container>
+                <Image 
+                    style={imageStyle} 
+                    src={this.props.image.path} 
+                    onClick={() => this.props.click(this.props.image)} 
+                    rounded 
+                />
             </>
         );
     }
