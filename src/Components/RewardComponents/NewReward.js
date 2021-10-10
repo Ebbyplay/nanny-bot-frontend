f32aef76de5e5d2af235430b7fd091559064e303
ox7f <ox7f@protonmail.com>
2021-10-10 16:05:55 +0200

    Revert "newReward: view update nach dem erstellen"
    This reverts commit f6ea93aae89915e377b9ccfaf390405eaba94848.

--- src/Components/RewardComponents/NewReward.js
+++ src/Components/RewardComponents/NewReward.js
@@ -5,19 +5,6 @@ import { FaRegSave } from 'react-icons/fa';
 import { IoReturnUpBack } from 'react-icons/io5';
 
 class NewReward extends React.Component {
-    state = {
-        title: '',
-        cost: 0
-    }
-
-    /**
-     * is triggered when typing in input fields
-     * @param {*} e 
-     */
-     onChange = (e) => {
-        this.setState({ [e.target.name]: e.target.value });
-    }
-
     render() {
         return (
             <Container fluid>
@@ -27,14 +14,14 @@ class NewReward extends React.Component {
                     </div>
 
                     <div className="col-6 text-end">
-                        <button className="btn btn-sm btn-success" name={this.props.rewardId} onClick={() => this.props.create(this.state.title, this.state.cost)}><FaRegSave className="pb-1" />Speichern</button>
+                        <button className="btn btn-sm btn-success" name={this.props.rewardId} onClick={this.props.handleSaveReward}><FaRegSave className="pb-1" />Speichern</button>
                     </div>
                 </div>
                 <div>
                     <label htmlFor="title">Titel</label>
-                    <input type="text" className="form-control" name="title" onChange={this.onChange} value={this.state.title} />
+                    <input type="text" className="form-control" name="title" onChange={this.props.onChange} value={this.props.state.title} />
                     <label htmlFor="cost">Punkte</label>
-                    <input type="number" className="form-control w-25" name="cost" min="0" onChange={this.onChange} value={this.state.cost} />
+                    <input type="number" className="form-control w-25" name="cost" min="0" onChange={this.props.onChange} value={this.props.state.cost} />
                     <ImageSelector />
                 </div>
             </Container>
