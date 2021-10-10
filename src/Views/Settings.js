75e79418bcff3b3fa20da9c9e9d0743a53d86d58
ox7f <ox7f@protonmail.com>
2021-10-10 15:54:02 +0200

    create subaccounts - needs some refining but works

--- src/Views/Settings.js
+++ src/Views/Settings.js
@@ -1,16 +1,38 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import React from 'react';
-import { Container } from 'react-bootstrap';
-import Tab from 'react-bootstrap/Tab';
-import Tabs from 'react-bootstrap/Tabs';
+import { Container, Button, Tab, Tabs, Row, Col, Form } from 'react-bootstrap';
 
 import NannyForm from '../Components/SettingsForm';
+import NannyImageGrid from '../Components/ImageGrid';
+import { signupSub } from '../Utils/CallMaster';
 
 /**
  * path: /settings
  */
 class Settings extends React.Component {
+    state = {
+        createView: false,
+        name: '',
+        selectedImages: []
+    }
+
+    onChange = (e) => {
+        this.setState({ [e.target.name]: e.target.value });
+    }
+
+    toggleImage = (images) => {
+        this.setState({
+            selectedImages: images
+        })
+    }
+
+    toggleView = () => {
+        this.setState({
+            createView: !this.state.createView
+        })
+    }
+
     changeAvatar = () => {
         // TODO: 
         console.log('todo: open imageSelector')
@@ -19,6 +41,24 @@ class Settings extends React.Component {
     applyNewSettings = (newUserSettings) => {
         // TODO: backend call und werte uebernehmen
         console.log('todo: apply new settings', newUserSettings)
+    }
+
+    createUser = () => {
+        let password = '';
+
+        this.state.selectedImages.forEach((image) => {
+            password += image.name;
+        })
+
+        signupSub(this.props.user.id, this.state.name, password)
+        .then((res) => {
+            let newSubAccount = res.data;
+
+            if (!newSubAccount)
+                return;
+
+            this.props.rootchangehandler('subaccounts', this.props.subacounts.concat(newSubAccount));
+        })
     }
 
     render() {
@@ -38,9 +78,40 @@ class Settings extends React.Component {
                         </Tab>
                         <Tab eventKey="sub" title="Kinder">
                             <div style={style}>
-                                {this.props.subaccounts.map((user) => (
-                                    <NannyForm key={user.id} user={user} apply={this.applyNewSettings} />
-                                ))}
+                                {this.state.createView ? (
+                                    <Form>
+                                        <Row className="align-items-center">
+                                            <Col className="my-1">
+                                                <Form.Label>Benutzername</Form.Label>
+                                                <Form.Control name="name" type="username" placeholder="Name eingebene" value={this.state.name} onChange={this.onChange} />
+                                            </Col>
+                                        </Row>
+
+                                        <Row className="align-items-center">
+                                            <Col className="my-1">
+                                                <Form.Label>Bilder Passwort:</Form.Label>
+                                                <NannyImageGrid click={this.toggleImage} />
+                                            </Col>
+                                        </Row>
+
+                                        <Row className="align-items-center">
+                                            <Col className="my-1">
+                                                <Button variant="primary" onClick={this.createUser}>
+                                                    Erstellen
+                                                </Button>
+                                            </Col>
+                                        </Row>
+                                    </Form>
+                                )
+                                : (
+                                    <>
+                                        {this.props.subaccounts.map((user) => (
+                                            <NannyForm key={user.id} user={user} apply={this.applyNewSettings} />
+                                        ))}
+
+                                        <Button onClick={this.toggleView}>Account hinzufuegen</Button>
+                                    </>
+                                )}                       
                             </div>
                         </Tab>
                     </Tabs>
