77cb67f46d4e36d53b4081280397b0761ce9967f
Ebby <tim.eberhardt@gmail.com>
2021-10-11 17:09:55 +0200

    added components

---
+++ src/Views/AdminUserTasks.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class AdminUserTasks extends React.Component {
+
+    render() {
+
+        return (
+            <>
+            </>
+        )
+    }
+}
+export default AdminUserTasks;
