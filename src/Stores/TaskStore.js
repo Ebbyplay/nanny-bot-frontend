5ac07f2f91b6326e1e87d3cd4228d4c504c3dbf1
ox7f <ox7f@protonmail.com>
2021-10-26 20:47:19 +0200

    taskStore - grob

--- src/Stores/TaskStore.js
+++ src/Stores/TaskStore.js
@@ -1,5 +1,52 @@
-class TaskStore {
+import { makeAutoObservable } from 'mobx';
 
+// import { ApiService } from '../Services';
+
+class TaskStore {
+    constructor() {
+        makeAutoObservable(this);
+    }
+
+    tasks = [];
+
+    getTask(id) {
+        return this.tasks.find((task) => task.id === id)
+    }
+
+    setTasks(tasks) {
+        this.tasks = tasks;
+    }
+
+    unsetTasks() {
+        this.tasks = [];
+    }
+
+    addTask(task) {
+        // TODO: api call
+        this.tasks.push(task);
+    }
+
+    editTask(id, task) {
+        // api call
+        this.tasks[this.tasks.indexOf(this.tasks.find((task) => task.id === id))] = task;
+    }
+
+    removeTask(task) {
+        // api call
+        this.tasks.splice(this.tasks.indexOf(task), 1);
+    }
+
+    verifyTask(task) {
+        // api call
+    }
+
+    checkTask(task) {
+        // api call
+    }
+
+    uncheckTask(task) {
+        // api call
+    }
 }
 
 export default new TaskStore();
