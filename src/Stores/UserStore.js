2bf62de7339f29e40bbcb151c9e6009f78505ba7
ox7f <ox7f@protonmail.com>
2021-10-26 20:27:32 +0200

    userStore - grob

--- src/Stores/UserStore.js
+++ src/Stores/UserStore.js
@@ -1,14 +1,75 @@
 import { makeAutoObservable } from 'mobx';
 
+import { ApiService, StorageService } from '../Services';
+
 class UserStore {
     constructor() {
         makeAutoObservable(this);
     }
 
-    currentUser = null;
+    currentUser = StorageService.get('user');
+    mainAccounts = [];
+    subAccounts = [];
 
     setUser(user) {
         this.currentUser = user;
+        StorageService.set('user', user);
+    }
+
+    setMainAccounts(mainAccounts) {
+        this.mainAccounts = mainAccounts;
+    }
+
+    setSubAccounts(subAccounts) {
+        this.subAccounts = subAccounts;
+    }
+
+    unsetUser() {
+        this.currentUser = null;
+        StorageService.unset('user');
+    }
+
+    unsetMainAccounts() {
+        this.mainAccounts = [];
+    }
+
+    unsetSubAccounts() {
+        this.subAccounts = [];
+    }
+
+    addMainAccount(mainAccount) {
+        // TODO: api call => wenn success dann
+        this.mainAccounts.push(mainAccount);
+    }
+
+    addSubAccount(subAccount) {
+        // api call
+        this.subAccounts.push(subAccount);
+    }
+
+    removeMainAccount(mainAccount) {
+        // api call
+        this.mainAccounts.splice(this.mainAccounts.indexOf(mainAccount), 1);
+    }
+
+    removeSubAccount(subAccount) {
+        // api call
+        this.subAccounts.splice(this.subAccounts.indexOf(subAccount), 1);
+    }
+
+    editCurrentUser(currentUser) {
+        // api call
+        this.currentUser = currentUser;
+    }
+
+    editMainAccount(mainAccount) {
+        // api call
+        this.mainAccounts[this.mainAccounts.indexOf(mainAccount)] = mainAccount;
+    }
+
+    editSubAccount(subAccount) {
+        // api call
+        this.subAccounts[this.subAccounts.indexOf(subAccount)] = subAccount;
     }
 }
 
