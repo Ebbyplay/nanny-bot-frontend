2a3664c28508ded3ed663da8c41752c513eb3046
ox7f <ox7f@protonmail.com>
2021-10-27 19:08:44 +0200

    index update

--- src/Stores/index.js
+++ src/Stores/index.js
@@ -1,4 +1,5 @@
 export { default as AuthStore } from './AuthStore';
-export { default as UserStore } from './UserStore';
-export { default as TaskStore } from './TaskStore';
+export { default as CommonStore } from './CommonStore';
 export { default as RewardStore } from './RewardStore';
+export { default as TaskStore } from './TaskStore';
+export { default as UserStore } from './UserStore';
